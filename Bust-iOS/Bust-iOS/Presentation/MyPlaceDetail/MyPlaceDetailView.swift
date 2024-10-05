0b7b06b88be878790f661ce9fd95f7a267f3c853
kwonjeong <kwonjeong9470@gmail.com>
2024-10-05 16:50:06 +0900

    #8 feat: 내가 방문한 장소 뷰 ui 구현

---
+++ Bust-iOS/Bust-iOS/Presentation/MyPlaceDetail/MyPlaceDetailView.swift
@@ -0,0 +1,170 @@
+//
+//  MyPlaceDetailView.swift
+//  Bust-iOS
+//
+//  Created by KJ on 10/5/24.
+//
+
+import UIKit
+
+import SnapKit
+
+final class MyPlaceDetailView: UIView {
+    
+    // MARK: - UI Components
+    
+    private let navigationBar = {
+        let view = UIView()
+        view.backgroundColor = .white
+        return view
+    }()
+    
+    private let backButton = {
+        let button = UIButton()
+        button.setImage(.icArrowBackTouch, for: .normal)
+        return button
+    }()
+    
+    private let placeImageView = UIImageView(image: .imgNight)
+    
+    private let locationNameLabel = {
+        let label = UILabel()
+        label.text = "장소 이름"
+        label.font = .fontBust(.title1)
+        label.textColor = .black
+        return label
+    }()
+    
+    private let locationDetailLabel = {
+        let label = UILabel()
+        label.text = "상세 위치: 경기도 의정부시 어쩌구"
+        label.font = .fontBust(.body21)
+        label.textColor = .black
+        return label
+    }()
+    
+    private let descriptionLabel = {
+        let label = UILabel()
+        label.text = """
+                    아라야 ! 나랑 놀고 싶어도 그렇지 어떻게 꿈에서까지 나올 수 있니ㅣ?\n이바보 바보~~~~~\n
+                    우하하우히히 우하 하아후아후아
+                    """
+        label.font = .fontBust(.body11)
+        label.textColor = .black
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let myHintLabel = {
+        let label = UILabel()
+        label.text = "내가 작성한 힌트"
+        label.font = .fontBust(.body1)
+        label.textColor = .mainBlue
+        return label
+    }()
+    
+    private let myHintView = {
+        let view = UIView()
+        view.backgroundColor = .gray100
+        view.layer.cornerRadius = 8
+        view.layer.masksToBounds = true
+        return view
+    }()
+    
+    private let myHintTextLabel = {
+        let label = UILabel()
+        label.text = "나의 힌트"
+        label.font = .fontBust(.body11)
+        label.textColor = .black
+        return label
+    }()
+    
+    // MARK: - Properties
+    
+    // MARK: - Initializer
+    
+    // MARK: - View Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension MyPlaceDetailView {
+    
+    // MARK: - UI Components Property
+    
+    private func setUI() {
+        
+    }
+    
+    // MARK: - Layout Helper
+    
+    private func setLayout() {
+        
+        navigationBar.addSubview(backButton)
+        myHintView.addSubview(myHintTextLabel)
+        
+        addSubviews(navigationBar, placeImageView, locationNameLabel, locationDetailLabel,
+                    descriptionLabel, myHintLabel, myHintView)
+        
+        navigationBar.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(48)
+        }
+        
+        backButton.snp.makeConstraints {
+            $0.top.leading.equalToSuperview()
+        }
+        
+        placeImageView.snp.makeConstraints {
+            $0.top.equalTo(navigationBar.snp.bottom).offset(8)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(216)
+        }
+        
+        locationNameLabel.snp.makeConstraints {
+            $0.top.equalTo(placeImageView.snp.bottom).offset(20)
+            $0.leading.equalToSuperview().inset(20)
+        }
+        
+        locationDetailLabel.snp.makeConstraints {
+            $0.top.equalTo(locationNameLabel.snp.bottom).offset(8)
+            $0.leading.equalTo(locationNameLabel)
+        }
+        
+        descriptionLabel.snp.makeConstraints {
+            $0.top.equalTo(locationDetailLabel.snp.bottom).offset(16)
+            $0.horizontalEdges.equalToSuperview().inset(20)
+            $0.width.equalTo(335)
+            $0.height.equalTo(130)
+        }
+        
+        myHintLabel.snp.makeConstraints {
+            $0.top.equalTo(descriptionLabel.snp.bottom).offset(40)
+            $0.leading.equalTo(locationNameLabel)
+        }
+        
+        myHintView.snp.makeConstraints {
+            $0.top.equalTo(myHintLabel.snp.bottom).offset(12)
+            $0.horizontalEdges.equalToSuperview().inset(20)
+            $0.height.equalTo(48)
+        }
+        
+        myHintTextLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview().inset(16)
+        }
+    }
+    
+    // MARK: - Methods
+    
+    // MARK: - @objc Methods
+}

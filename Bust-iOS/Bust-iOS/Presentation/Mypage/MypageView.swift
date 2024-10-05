90790571fb81f26e9039d1c9422f1d78efd0142c
kwonjeong <kwonjeong9470@gmail.com>
2024-10-05 21:14:00 +0900

    #12 feat: 마이페이지 뷰 구현

---
+++ Bust-iOS/Bust-iOS/Presentation/Mypage/MypageView.swift
@@ -0,0 +1,157 @@
+//
+//  MypageView.swift
+//  Bust-iOS
+//
+//  Created by KJ on 10/5/24.
+//
+
+import UIKit
+
+import SnapKit
+
+final class MypageView: UIView {
+    
+    // MARK: - UI Components
+    
+    private let myProfileView = {
+        let view = UIView()
+        view.backgroundColor = .lightBlue
+        return view
+    }()
+    
+    private let myProfileLabel = {
+        let label = UILabel()
+        label.text = "나의 프로필"
+        label.textColor = .black
+        label.font = .fontBust(.subtitle1)
+        return label
+    }()
+    
+    let settingButton = {
+        let button = UIButton()
+        button.setImage(.icSetting, for: .normal)
+        return button
+    }()
+    
+    private let profileImageView = UIImageView(image: .imgProfile)
+    
+    private let userNameLabel = {
+        let label = UILabel()
+        label.text = "부스트 님"
+        label.textColor = .mainBlue
+        label.font = .fontBust(.body1)
+        return label
+    }()
+    
+    private let myPlaceListView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        return view
+    }()
+    
+    private let myPlaceLabel = {
+        let label = UILabel()
+        label.text = "나의 장소리스트"
+        label.font = .fontBust(.subtitle1)
+        label.textColor = .black
+        return label
+    }()
+    
+    lazy var myPlaceCollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: SizeLiterals.Screen.screenWidth - 40, height: 96)
+        layout.minimumInteritemSpacing = 12
+        layout.scrollDirection = .vertical
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.showsVerticalScrollIndicator = false
+        cv.backgroundColor = .clear
+        cv.register(
+            MyPageListCollectionViewCell.self,
+            forCellWithReuseIdentifier: MyPageListCollectionViewCell.className
+        )
+        return cv
+    }()
+    
+    // MARK: - Properties
+    
+    // MARK: - Initializer
+    
+    // MARK: - View Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension MypageView {
+    
+    // MARK: - UI Components Property
+    
+    private func setUI() {
+        
+        self.backgroundColor = .clear
+        
+    }
+    
+    // MARK: - Layout Helper
+    
+    private func setLayout() {
+        
+        addSubviews(myProfileView, myPlaceListView)
+        myProfileView.addSubviews(myProfileLabel, settingButton, profileImageView, userNameLabel)
+        myPlaceListView.addSubviews(myPlaceLabel, myPlaceCollectionView)
+        
+        myProfileView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(284)
+        }
+        
+        myProfileLabel.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide).inset(20)
+            $0.leading.equalToSuperview().inset(20)
+        }
+        
+        settingButton.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide).inset(8)
+            $0.trailing.equalToSuperview().inset(6)
+        }
+        
+        profileImageView.snp.makeConstraints {
+            $0.top.equalTo(settingButton.snp.bottom).offset(8)
+            $0.size.equalTo(110)
+            $0.centerX.equalToSuperview()
+        }
+        
+        userNameLabel.snp.makeConstraints {
+            $0.top.equalTo(profileImageView.snp.bottom).offset(16)
+            $0.centerX.equalToSuperview()
+        }
+        
+        myPlaceListView.snp.makeConstraints {
+            $0.top.equalTo(myProfileView.snp.bottom)
+            $0.horizontalEdges.equalToSuperview()
+            $0.bottom.equalTo(safeAreaLayoutGuide).inset(20)
+        }
+        
+        myPlaceLabel.snp.makeConstraints {
+            $0.top.leading.equalToSuperview().inset(20)
+        }
+        
+        myPlaceCollectionView.snp.makeConstraints {
+            $0.top.equalTo(myPlaceLabel.snp.bottom).offset(12)
+            $0.horizontalEdges.equalToSuperview()
+            $0.bottom.equalToSuperview().inset(12)
+        }
+    }
+    
+    // MARK: - Methods
+    
+    // MARK: - @objc Methods
+}
---
+++ Bust-iOS/Bust-iOS/Presentation/Mypage/MypageViewController.swift
@@ -0,0 +1,95 @@
+//
+//  MypageViewController.swift
+//  Bust-iOS
+//
+//  Created by KJ on 10/5/24.
+//
+
+import UIKit
+
+import Moya
+import SnapKit
+
+final class MypageViewController: UIViewController {
+    
+    // MARK: - UI Components
+    
+    private let myPageView = MypageView()
+    
+    // MARK: - Properties
+    
+    // MARK: - Initializer
+    
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        setLayout()
+        setDelegate()
+        setAddTarget()
+        
+        self.navigationController?.navigationBar.isHidden = true
+    }
+}
+
+extension MypageViewController {
+    
+    // MARK: - UI Components Property
+    
+    private func setUI() {
+        
+        self.view.backgroundColor = .white
+        
+    }
+    
+    // MARK: - Layout Helper
+    
+    private func setLayout() {
+        
+        self.view.addSubview(myPageView)
+        
+        myPageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    // MARK: - Methods
+    
+    private func setDelegate() {
+        myPageView.myPlaceCollectionView.delegate = self
+        myPageView.myPlaceCollectionView.dataSource = self
+    }
+    
+    private func setAddTarget() {
+        myPageView.settingButton.addTarget(self, action: #selector(settingButtonDidTap), for: .touchUpInside)
+    }
+    
+    // MARK: - @objc Methods
+    
+    @objc
+    private func settingButtonDidTap() {
+//        self.navigationController?.pushViewController(<#T##viewController: UIViewController##UIViewController#>, animated: <#T##Bool#>)
+    }
+}
+
+extension MypageViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        let recordDetailData = recordData.diaries[indexPath.item]
+        self.navigationController?.pushViewController(MyPlaceDetailViewController(), animated: true)
+    }
+}
+
+extension MypageViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell =  MyPageListCollectionViewCell.dequeueReusableCell(collectionView: myPageView.myPlaceCollectionView, indexPath: indexPath)
+//        cell.bindRecord(recordData.diaries[indexPath.item])
+        return cell
+    }
+}

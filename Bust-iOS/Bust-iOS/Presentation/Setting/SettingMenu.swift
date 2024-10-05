945a62d915281048e1c6b840469066ac9e2d62f7
ahra_ <ahravelop@gmail.com>
2024-10-05 18:42:45 +0900

    #12 feat: 설정뷰 구현

---
+++ Bust-iOS/Bust-iOS/Presentation/Setting/SettingMenu.swift
@@ -0,0 +1,26 @@
+//
+//  SettingMenu.swift
+//  Bust-iOS
+//
+//  Created by 고아라 on 10/5/24.
+//
+
+struct SettingMenu {
+    let title: String
+}
+
+extension SettingMenu {
+    static func serviceMenu() -> [SettingMenu] {
+        return [
+            SettingMenu(title: "이용약관"),
+            SettingMenu(title: "개인정보처리방침"),
+        ]
+    }
+    
+    static func accountMenu() -> [SettingMenu] {
+        return [
+            SettingMenu(title: "로그아웃"),
+            SettingMenu(title: "탙퇴하기"),
+        ]
+    }
+}
